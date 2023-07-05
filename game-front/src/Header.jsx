93c245822fb52ced986394b09a4e63963381adb4
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2023-07-05 16:13:57 +0900

    로그인여부 감지

--- game-front/src/Header.jsx
+++ game-front/src/Header.jsx
@@ -1,22 +1,28 @@
 import { Box, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const getUser = localStorage.getItem('game-id');
+  const navigate = useNavigate();
   console.log(getUser, 'agsd');
   return (
     <Box>
-      <Link to='/'>홈</Link> <Link to='/signup'>회원가입</Link>{' '}
-      {getUser !== null || undefined ? (
-        <Button
-          onClick={() => {
-            localStorage.removeItem('game-id');
-          }}
-        >
-          로그아웃
-        </Button>
+      {getUser !== null && getUser !== undefined ? (
+        <>
+          <Link to='/'>홈</Link>{' '}
+          <Button
+            onClick={() => {
+              localStorage.removeItem('game-id');
+              navigate('/');
+            }}
+          >
+            로그아웃
+          </Button>
+        </>
       ) : (
-        <Link to='/login'>로그인</Link>
+        <>
+          <Link to='/'>홈</Link> <Link to='/signup'>회원가입</Link> <Link to='/login'>로그인</Link>
+        </>
       )}
       {/* <Link to='/game'>게임하기</Link> */}
     </Box>
--- game-front/src/Login.jsx
+++ game-front/src/Login.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import axios from 'axios';
 import { Box, Button, Container, TextField } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
+import GameStartView from './GameStartView';
 
 function LoginForm() {
   const [nickname, setNickname] = useState('');
   const [password, setPassword] = useState('');
+
+  const getUser = localStorage.getItem('game-id');
 
   const navigate = useNavigate();
 
@@ -24,57 +27,34 @@ function LoginForm() {
 
   return (
     <Container maxWidth='sm' sx={{ mt: 10 }}>
-      <h2>로그인</h2>
-      <TextField
-        label='사용자 닉네임'
-        value={nickname}
-        onChange={e => setNickname(e.target.value)}
-        fullWidth
-        margin='normal'
-      />
-      <TextField
-        label='비밀번호'
-        type='password'
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        fullWidth
-        margin='normal'
-      />
-      <Button variant='contained' onClick={handleLogin}>
-        로그인하기
-      </Button>
-      <Link to='/signup'>
-        <Button>회원이 아니라면 가입하러 가기</Button>
-      </Link>
-      {/* <Box
-        sx={{
-          mt: '150px',
-          display: 'flex',
-          flexFlow: 'column nowrap',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Link to='/game'>
-          <Button
-            sx={{
-              fontSize: '30px',
-              fontWeight: 'bold',
-              bgcolor: 'blue',
-              color: '#fff',
-              p: '10px 30px',
-              transition: 'unset',
-              '&:hover': {
-                bgcolor: '#fff',
-                border: '3px solid blue',
-                color: 'blue',
-              },
-            }}
-          >
-            게임 시작 GO !
+      {getUser === null || getUser === undefined ? (
+        <>
+          <h2>로그인</h2>
+          <TextField
+            label='사용자 닉네임'
+            value={nickname}
+            onChange={e => setNickname(e.target.value)}
+            fullWidth
+            margin='normal'
+          />
+          <TextField
+            label='비밀번호'
+            type='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            fullWidth
+            margin='normal'
+          />
+          <Button variant='contained' onClick={handleLogin}>
+            로그인하고 게임하러 가기
           </Button>
-        </Link>
-      </Box> */}
+          <Link to='/signup'>
+            <Button>회원이 아니라면 가입하러 가기</Button>
+          </Link>
+        </>
+      ) : (
+        <GameStartView />
+      )}
     </Container>
   );
 }
